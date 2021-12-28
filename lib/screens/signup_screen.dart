d4631c4dad3720edf9787ad18d3469d22fc0db7c
Sanat Thakur <sanatthakur2002@gmail.com>
2021-12-28 16:58:29 +0530

    working on signup screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:grabcab/painters/signup_painter.dart';
 import 'package:lottie/lottie.dart';
@@ -47,21 +49,39 @@ class _SignUpState extends State<SignUp> {
                 child: Row(
                   children: [
                     "Welcome to Grab".text.textStyle(TextStyle(fontSize: 30,fontFamily:GoogleFonts.arsenal().fontFamily)).make(),
-                    "Cab".text.blue700.extraBold.textStyle(TextStyle(fontSize: 30,fontFamily:GoogleFonts.arsenal().fontFamily)).make(),
+                    "Cab".text.purple500.extraBold.textStyle(TextStyle(fontSize: 30,fontFamily:GoogleFonts.arsenal().fontFamily)).make(),
                     "!!!".text.textStyle(TextStyle(fontSize: 30,fontFamily:GoogleFonts.arsenal().fontFamily)).make(),
                   ],
                 ),
               ),
               Positioned(
-                top:size.height*.5,
-                left: 0,
-                right: 0,
+                top:size.height*.52,
+                left: 40,
+                right: 40,
                 child: Column(
                   children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "helo",
-                        label: "le".text.make()
+                    Container(
+                      decoration: BoxDecoration(
+                        color:Colors.purple.shade400,
+                        borderRadius: BorderRadius.all(Radius.circular(50))
+                      ),
+                      child: TextFormField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        cursorColor: Colors.black,
+                        cursorRadius: Radius.circular(10),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person,color: Colors.white,),
+                          hintText: "Jhonny Depp",
+                          label: "Username".text.white.make(),
+                          hintStyle: TextStyle(color: Colors.white),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent)),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent))
+                        ),
+                        
                       ),
                     )
                   ],
