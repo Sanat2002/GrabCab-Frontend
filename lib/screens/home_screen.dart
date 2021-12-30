45a6a61ccfe4e3590e85a90f50a09a4d19876a5c
Sanat Thakur <sanatthakur2002@gmail.com>
2021-12-30 12:27:00 +0530

    working on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:grabcab/screens/signin_screen.dart';
 import 'package:grabcab/services/authentication.dart';
@@ -20,20 +22,73 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.purple.shade400,
         title:"GrabCab".text.xl4.make().px(size.width*.15)
       ),
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right:0,
-            child: TextFormField(
-              onChanged: (value){
-
-              },
-
-            ).px(30)
-          )
-        ],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                right:0,
+                child: TextFormField(
+                  cursorColor: Colors.purple.shade300,
+                  cursorHeight: 25,
+                  style: TextStyle(
+                    color: Colors.purple.shade400,
+                  ),
+                  onChanged: (value){
+        
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Search for cabs",
+                    hintStyle: TextStyle(
+                      color: Colors.black45,
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple)
+                    )
+                  ),
+                ).px(30)
+              ),
+              Positioned(
+                top: 70,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: ListView(
+                  children: [
+                    Container(
+                      height: size.height*.35,
+                      decoration: BoxDecoration(
+                        color: Colors.purple.shade200,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: size.height*.28,
+                            decoration:BoxDecoration(
+                              color: Colors.black,
+                              borderRadius:BorderRadius.all(Radius.circular(10))
+                            )
+                          ).px(10).py(4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              "Audi".text.xl3.make(),
+                              "Rs 2000".text.xl3.make(),
+                            ],
+                          ).py(7)
+                        ],
+                      ),
+                    ).px(17)
+                  ],
+                ))
+            ],
+          ),
+        ),
       ),
       drawer: Drawer(),
     );
