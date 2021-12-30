88e04fe6c82eba8de44a5f8e070e0c742a667d4c
Sanat Thakur <sanatthakur2002@gmail.com>
2021-12-30 21:33:17 +0530

    working on home screen

--- lib/screens/mygrabs_screen.dart
+++ lib/screens/mygrabs_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -11,13 +11,38 @@ class MyGrabs extends StatefulWidget {
 }
 
 class _MyGrabsState extends State<MyGrabs> {
+
   @override
   Widget build(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple.shade400,
         title: "MY Grabs".text.xl3.make().px(70),
       ),
+      body: Container(
+        height: size.height*.1 ,
+        decoration: BoxDecoration(
+          color: Colors.black26,
+          borderRadius: BorderRadius.all(Radius.circular(20))
+        ),
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            20.widthBox,
+            CircleAvatar(radius: 33,),
+            20.widthBox,
+            SizedBox(width:size.width*.34,child: "Audi".text.xl2.make()),
+            50.widthBox,
+            TextButton(
+              onPressed: (){
+
+              }, 
+              child: "Return".text.rose600.xl2.make())
+          ],
+        ),
+      ).px(10).py(8),
     );
   }
 }
