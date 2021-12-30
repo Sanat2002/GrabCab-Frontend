73d163e0b6bbaf0a4d01aa08b1d489f95b600c43
Sanat Thakur <sanatthakur2002@gmail.com>
2021-12-30 20:02:13 +0530

    working on home screen

--- lib/screens/cabdetail_screen.dart
+++ lib/screens/cabdetail_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CabDetail extends StatefulWidget {
   const CabDetail({ Key? key }) : super(key: key);
@@ -11,8 +15,82 @@ class CabDetail extends StatefulWidget {
 class _CabDetailState extends State<CabDetail> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Hero(tag: "el", child: Image.asset("assets/del.JPG")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Hero(tag:"el",transitionOnUserGestures: true,child: Padding(
+            padding: EdgeInsets.only(top: 36),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Vx.gray200,
+                borderRadius: BorderRadius.all(Radius.elliptical(20, 40))
+              ),
+              height:size.height*.4 ,
+              width: size.width*.9,
+              child: Image.asset("assets/del.JPG")).px(20),
+          )),
+          Divider(color: Colors.black),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              "Brand".text.xl3.extraBold.make(),
+              "Audi".text.xl3.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).green600.make()
+            ],
+          ).p(4).py(4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              "Model".text.xl3.extraBold.make(),
+              "AudiR8".text.xl3.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).green600.make()
+            ],
+          ).p(4).py(4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              "Odometer".text.xl3.extraBold.make(),
+              "1112".text.xl3.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).green600.make()
+            ],
+          ).p(4).py(4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              "Price".text.xl3.extraBold.make(),
+              "\$2000".text.xl3.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).green600.make()
+            ],
+          ).p(4).py(4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              "Rent".text.xl3.extraBold.make(),
+              "\$20/day".text.xl3.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).green600.make()
+            ],
+          ).p(4).py(4),
+        ],
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            style:ButtonStyle(
+              backgroundColor:MaterialStateProperty.all(Colors.purple.shade300),
+            ),
+            onPressed: (){
+
+            },
+            child: "Rent".text.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).xl3.make()),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:MaterialStateProperty.all(Colors.purple.shade600)
+            ),
+            onPressed: (){
+
+            },
+            child: "Buy".text.textStyle(TextStyle(fontFamily: GoogleFonts.davidLibre().fontFamily)).xl3.make())
+        ],
+      ).py(10),
     );
   }
 }
