a5f75ad7d036fbaf953cb45447d9b168e650947e
Sanat Thakur <sanatthakur2002@gmail.com>
2021-12-30 11:34:28 +0530

    working on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,13 +13,29 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body:ElevatedButton(
-        onPressed: () async{
-          await AuthenticationService().signout();
-          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>const SignIn()), (route) => false);
-        },
-        child: "Signout".text.make(),).centered()
+      appBar: AppBar(
+        backgroundColor: Colors.purple.shade400,
+        title:"GrabCab".text.xl4.make().px(size.width*.15)
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right:0,
+            child: TextFormField(
+              onChanged: (value){
+
+              },
+
+            ).px(30)
+          )
+        ],
+      ),
+      drawer: Drawer(),
     );
   }
 }
